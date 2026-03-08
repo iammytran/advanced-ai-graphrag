c39bb11e14a81583b517959210e8f27b8f7acf97
KMON\ASUS <dangkhoa3122003@gmail.com>
2026-03-08 12:54:47 +0700

    feat: Implement environment variable-based configuration and add RAG evaluator file.

--- backend/config/config.py
+++ backend/config/config.py
@@ -6,5 +6,6 @@ load_dotenv()
 
 TEMPERATURE = os.getenv("TEMPERATURE", 0.7)
 
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 OPENAI_MODEL = os.getenv("OPENAI_MODEL", "gpt-4.1-mini")
 EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL", "text-embedding-3-small")
